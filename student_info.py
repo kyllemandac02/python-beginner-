0cbf295417824e7b0d4866234c89e032355f3575
kyllemandac02 <kyllemandacins@gmail.com>
2026-09-15 23:13:59 +0800

    Add student info program

---
+++ student_info.py
@@ -0,0 +1,14 @@
+Name = input("Enter your name: ")
+Age = int(input("Enter your age: "))
+Course = input("Enter your course: ")
+Section = input("Enter your section: ”)
+Favorite_language = input("Enter your favorite programming language: ")      
+
+print("Name", name)
+print("Age", age)
+print("Course", course)
+print("Section", section)
+print("Favorite programming language", favorite_language)
+
+
+                
